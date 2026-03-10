d4c4019ec8b117d73b5c7ca6d93d6c4a2cce108b
Vishwajeet Yadav <yadavvishwajeet2004@gmail.com>
2026-03-10 13:16:17 +0530

    v2.1 pwa

--- web/static/sw.js
+++ web/static/sw.js
@@ -1,42 +1,22 @@
-
-const CACHE_NAME = 'temple-app-v2';
+const CACHE_NAME = "temple-app-v1";
 const urlsToCache = [
-    '/',
-    '/login',
-    '/donations',
-    '/expenses',
-    '/festivals',
-    '/reports',
-    '/static/css/global.css',
-    '/static/manifest.json'
+    "/",
+    "/static/css/global.css",
+    "/static/images/temple-bg.jpg"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-            .then(() => self.skipWaiting())
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
     );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-            .catch(() => caches.match('/'))
-    );
-});
-
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        }).then(() => self.clients.claim())
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
     );
 });
